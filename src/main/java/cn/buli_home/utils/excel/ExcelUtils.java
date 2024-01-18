4b1275231432a8c75b1432bb0f3e650ba3cf73a9
Mustard <mustard_gxg@foxmail.com>
2024-01-18 10:55:46 +0800

    Excel 文件读取

--- src/main/java/cn/buli_home/utils/excel/ExcelUtils.java
+++ src/main/java/cn/buli_home/utils/excel/ExcelUtils.java
@@ -2,12 +2,20 @@ package cn.buli_home.utils.excel;
 
 import com.alibaba.excel.EasyExcel;
 
+import java.io.InputStream;
+
 public class ExcelUtils {
 
     public static void read(String filePath, int sheetNo, Class clazz, ExcelDataCallback<?> callback) {
         ExcelDataListener<ExcelData> dataListener = new ExcelDataListener(callback);
 
         EasyExcel.read(filePath, clazz, dataListener).sheet(sheetNo).doRead();
+    }
+
+    public static void read(InputStream in, int sheetNo, Class clazz, ExcelDataCallback<?> callback) {
+        ExcelDataListener<ExcelData> dataListener = new ExcelDataListener(callback);
+
+        EasyExcel.read(in, clazz, dataListener).sheet(sheetNo).doRead();
     }
 
 }
