3e833eb19ff1da44b7f4de296123bdf6db19406a
Mustard <mustard_gxg@foxmail.com>
2024-01-19 10:20:51 +0800

    excel 填充模板

--- src/main/java/cn/buli_home/utils/excel/ExcelDataListener.java
+++ src/main/java/cn/buli_home/utils/excel/ExcelDataListener.java
@@ -1,23 +1,19 @@
 package cn.buli_home.utils.excel;
 
 import com.alibaba.excel.context.AnalysisContext;
-import com.alibaba.excel.read.listener.ReadListener;
+import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.util.ListUtils;
-import com.alibaba.fastjson2.JSON;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.Objects;
 
-public class ExcelDataListener<T> implements ReadListener<T> {
-    private static final Logger log = LogManager.getLogger(ExcelDataListener.class);
+public class ExcelDataListener<T> extends AnalysisEventListener<T> {
 
     // 有个很重要的点 DemoDataListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
     /**
-     * 每隔5条存储数据库，实际使用中可以100条，然后清理list ，方便内存回收
+     * 每隔 3000 条，然后清理list ，方便内存回收
      */
-    private static final int BATCH_COUNT = 100;
+    private static final int BATCH_COUNT = 3000;
     /**
      * 缓存的数据
      */
@@ -26,7 +22,8 @@ public class ExcelDataListener<T> implements ReadListener<T> {
     private ExcelDataCallback<T> callback;
 
 
-    public ExcelDataListener() {}
+    public ExcelDataListener() {
+    }
 
     public ExcelDataListener(ExcelDataCallback<T> callback) {
         this.callback = callback;
@@ -40,7 +37,6 @@ public class ExcelDataListener<T> implements ReadListener<T> {
      */
     @Override
     public void invoke(T data, AnalysisContext context) {
-        log.info("解析到一条数据:{}", JSON.toJSONString(data));
         cachedDataList.add(data);
         // 达到BATCH_COUNT了，需要去存储一次数据库，防止数据几万条数据在内存，容易OOM
         if (cachedDataList.size() >= BATCH_COUNT) {
@@ -67,8 +63,6 @@ public class ExcelDataListener<T> implements ReadListener<T> {
         }
 
         cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
-
-        log.info("所有数据解析完成！");
     }
 
 }
